266446c668eb29e50d79cae4e67fa55f889f72ff
Marco Rauseo <rauseom@libero.it>
2025-06-15 18:45:15 +0200

    refactoring

--- src/main/java/com/contentwise/reco/controller/MovieController.java
+++ src/main/java/com/contentwise/reco/controller/MovieController.java
@@ -1,11 +1,14 @@
 package com.contentwise.reco.controller;
 
 import com.contentwise.reco.dto.MovieDto;
+import com.contentwise.reco.service.MovieSearchService;
 import com.contentwise.reco.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/movies")
@@ -23,5 +26,16 @@ public class MovieController {
             @RequestParam(defaultValue = "20") int size) {
 
         return service.list(genre, minRating, maxRating, PageRequest.of(page, size));
+    }
+
+    private final MovieSearchService movieSearchService;
+
+    @GetMapping("/search")
+    public List<MovieDto> search(@RequestParam(required = false) String title,
+                                 @RequestParam(required = false) List<String> genres,
+                                 @RequestParam(required = false) List<String> words,
+                                 @RequestParam(defaultValue = "0") int page,
+                                 @RequestParam(defaultValue = "10") int size) {
+        return movieSearchService.search(title, genres, words, page, size);
     }
 }
